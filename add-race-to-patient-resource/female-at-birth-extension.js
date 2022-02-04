a38641b409200f975c8a13ea0ef883c6b8b460e8
Dan Heslinga <dan@heslinga.us>
2022-02-04 12:51:35 -1000

    clean up comment clutter and cread race-top-level-categories.js and female-at-birth-extension.js

---
+++ add-race-to-patient-resource/female-at-birth-extension.js
@@ -0,0 +1,9 @@
+              // use this to add a female-at-birth extension for testing purposes  // DH
+              // if (pt.hasOwnProperty("extension")) {
+              //   console.log("this patient resource has an extension");
+              //   pt["extension"].push(femaleAtBirth);
+              // } else {
+              //   console.log("this patient resource does not have an extension");
+              //   pt["extension"] = [femaleAtBirth];
+              // end
+              // this is the actual code:
---
+++ add-race-to-patient-resource/race-top-level-categories.js
@@ -0,0 +1,22 @@
+// here are the top level categories
+// 2054-5	Black or African American
+// 2028-9	Asian
+// 2076-8	Native Hawaiian or Other Pacific Islander
+// 2106-3	White
+// 1002-5	American Indian or Alaska Native
+// see https://www.hl7.org/fhir/v3/Race/cs.html
+// e.g. white:
+// let white = [
+//   {
+//     url: "http://hl7.org/fhir/StructureDefinition/us-core-race",
+//     valueCodeableConcept: {
+//       coding: [
+//         {
+//           system: "http://hl7.org/fhir/v3/Race",
+//           code: "2106-3",
+//           display: "White",
+//         },
+//       ],
+//     },
+//   },
+// ];
